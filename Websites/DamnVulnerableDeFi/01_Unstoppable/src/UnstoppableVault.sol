756994ce489f87370578b9f2883d70354e3efbfa
J4X-98 <y.buchrieser@gmail.com>
2023-05-24 19:52:17 +0200

    Finished Unstoppable from DVDF

--- Websites/DamnVulnerableDeFi/01_Unstoppable/src/UnstoppableVault.sol
+++ Websites/DamnVulnerableDeFi/01_Unstoppable/src/UnstoppableVault.sol
@@ -94,15 +94,21 @@ contract UnstoppableVault is IERC3156FlashLender, ReentrancyGuard, Owned, ERC462
         bytes calldata data
     ) external returns (bool) {
         if (amount == 0) revert InvalidAmount(0); // fail early
+        
         if (address(asset) != _token) revert UnsupportedCurrency(); // enforce ERC3156 requirement
+
         uint256 balanceBefore = totalAssets();
         if (convertToShares(totalSupply) != balanceBefore) revert InvalidBalance(); // enforce ERC4626 requirement
+
         uint256 fee = flashFee(_token, amount);
+
         // transfer tokens out + execute callback on receiver
         ERC20(_token).safeTransfer(address(receiver), amount);
+
         // callback must return magic value, otherwise assume it failed
         if (receiver.onFlashLoan(msg.sender, address(asset), amount, fee, data) != keccak256("IERC3156FlashBorrower.onFlashLoan"))
             revert CallbackFailed();
+        
         // pull amount + fee from receiver, then pay the fee to the recipient
         ERC20(_token).safeTransferFrom(address(receiver), address(this), amount + fee);
         ERC20(_token).safeTransfer(feeRecipient, fee);
